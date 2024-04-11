72e4eb03182f7abf0cab21378dc1716e88021e33
charles chien <charleschien7996@gmail.com>
2024-04-11 15:44:58 +0800

    feat: add shared secret

--- ios_example/ios_example/KgSDKService.swift
+++ ios_example/ios_example/KgSDKService.swift
@@ -9,6 +9,7 @@ class KgSDKService : ObservableObject{
     private var methodChannel: FlutterMethodChannel?
     private var openVerifyPageCallback: ((_ result: @escaping FlutterResult) -> Void)?
     
+    
     private init (flutterViewController: FlutterViewController? = nil, methodChannel: FlutterMethodChannel? = nil, openVerifyPageCallback: ( (_: FlutterResult) -> Void)? = nil) {
         self.flutterViewController = flutterViewController
         self.methodChannel = methodChannel
@@ -27,33 +28,60 @@ class KgSDKService : ObservableObject{
     }
     
     private func setUpMethodChannel() {
+        let sharedSecret = fetchSharedSecret()
         // Send initial parameter to Flutter.
-        let initParam: [String: Any] = [
+        var initParam: [String: Any] = [
             "appName": "TWMTEST",
             "walletConfig": [
-                "maxWallet": 100
+                "maxWallet": 100,
+                "enableAutoSssSignUp": true,
+                "enableSSS":true,
+                "allRpcs": ["ethereum"]
             ],
             "themeData": [
                 "primaryValue": "FFFFC211"
             ],
             "flavorEnum": "dev",
             "clientId": "def3b0768f8f95ffa0be37d0f54e2064",
-            "clientToken": "eyJhbGciOiJSUzI1NiIsImtpZCI6ImRlbW8ta2V5IiwidHlwIjoiSldUIn0.eyJhdWQiOiJodHRwczovL2tyeXB0b2dvLmNvbSIsImV4cCI6MjAyNzQwOTg2MiwiaXNzIjoiaHR0cHM6Ly9hdXRoLmtyeXB0b2dvLmNvbSIsInN1YiI6InRlc3QtdXNlciJ9.Kmbblm_cUJNpoRImSRQmb83ljY35Kn-ZcA5SBy5WOPqqL6T42YVDJFMyOAp05j3aFfUIZxCOqQAFuT23bC53jZM9SOZjz9cmwqHOE6D9wzk6Y2gwdOABSIeEet2nGzXfoHcPR1GLXJYdnOWYdh9ZivE4dtH4wGRO-eiOUoJX_kxSunBk1XanG6T3BcCDduEd-jxHTBSoi2fcMU_KfDVA9ZTc3kwzzYq3qQUMu8lBIBUQYqeV3S4M29AMn1gUAlP5Z1oKuQZzYEM3jLxAkN9hls1fMavsfi2VGYK87UE7THyWmTgMU9BDNzk3DrT7Wcxc1DOhwotyrTtep8BQkjsCJw"
+            "clientToken": "eyJhbGciOiJSUzI1NiIsImtpZCI6ImRlbW8ta2V5IiwidHlwIjoiSldUIn0.eyJhdWQiOiJodHRwczovL2tyeXB0b2dvLmNvbSIsImV4cCI6MjAyNzQwOTg2MiwiaXNzIjoiaHR0cHM6Ly9hdXRoLmtyeXB0b2dvLmNvbSIsInN1YiI6InRlc3QtdXNlcjIifQ.iLJy5JM9uY0Qqtsngyjvx9coU3e5ILgVyjL2IoFdNnqVvDK6zvF28Oy_Svm_9klLhQT-v0We9IwuNhAHOMQ5hHZ4qKna0iBuAy6K2Ooj9N2_1sn4Wxfj-QIQQc1Sx3rSaVuxES2qGyccVH460-KK-jXLsDde7WZNgyJdXlsTwnOUM62w-VODhhRRpoqsmNr_aTHPhsF5XAYCCZVjZss2L4x1XQWMZff0Cue_cQl3Beh3NU8qHpGdTBhIZHH6YjnTdzBtJSCapzU-YFwNArLlnZRcVG2ZtLK_2iA15918w8GtckwHlEDkvl3f49F5oKiY5CiTuU2_RIKP1AoMkvl-5g"
         ]
-
+        if sharedSecret != nil {
+            initParam["sharedSecret"] = sharedSecret
+        }
+        
         methodChannel?.invokeMethod("getInitialParam", arguments: initParam)
-
+        
         // Set method call handler for future Flutter-to-native calls.
         methodChannel?.setMethodCallHandler { [self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
             switch call.method {
-                    case "closeFlutterView":
-                        flutterViewController!.dismiss(animated: true, completion: nil)
-                    case "openVerifyPage":
-                        self.openVerifyPageCallback?(result)
-                    default:
-                        result(FlutterMethodNotImplemented)
-                    }
+            case "closeFlutterView":
+                flutterViewController!.dismiss(animated: true, completion: nil)
+            case "openVerifyPage":
+                self.openVerifyPageCallback?(result)
+            case "updateSharedSecret":
+                print(call.arguments)
+                let sharedSecret = (call.arguments as? [String: String])?["sharedSecret"]
+                let isSuccess = self.updateSharedSecret(sharedSecret: sharedSecret)
+                result(isSuccess)
+            default:
+                result(FlutterMethodNotImplemented)
+            }
         }
+    }
+    
+    // To be implemented: update to server
+    func updateSharedSecret(sharedSecret: String?) -> Bool {
+        guard let secret = sharedSecret else {
+            return false
+        }
+        UserDefaults.standard.set(secret, forKey: "sharedSecret")
+        return true
+    }
+    
+    
+    // To be implemented: fetch from server
+    func fetchSharedSecret() -> String? {
+        return UserDefaults.standard.string(forKey: "sharedSecret")
     }
     
     
@@ -62,7 +90,7 @@ class KgSDKService : ObservableObject{
         if flutterViewController == nil {
             initializeFlutterViewController(flutterEngine:flutterEngine!)
         }
-
+        
         
         flutterViewController?.modalPresentationStyle = .overCurrentContext
         flutterViewController?.isViewOpaque = false
@@ -73,13 +101,13 @@ class KgSDKService : ObservableObject{
         return FlutterViewController(engine: flutterEngine!, nibName: nil, bundle: nil)
     }
     
-
+    
     func callKgSDK(funcName: String, completion: @escaping (Any?) -> Void) {
         // Create the FlutterViewController.
         let flutterViewController = FlutterViewController(
-          engine: flutterEngine!,
-          nibName: nil,
-          bundle: nil)
+            engine: flutterEngine!,
+            nibName: nil,
+            bundle: nil)
         
         let channel = FlutterMethodChannel(name: "com.kryptogo.sdk/channel", binaryMessenger: flutterViewController.binaryMessenger)
         channel.invokeMethod(funcName, arguments: nil,result: { (result) in
@@ -87,6 +115,6 @@ class KgSDKService : ObservableObject{
             completion(result)
             
         })
-      }
-
+    }
+    
 }
