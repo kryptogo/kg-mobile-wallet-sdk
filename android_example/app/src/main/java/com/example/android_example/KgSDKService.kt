7ed48de1250cad20a6259829111d24a7b8641317
DORA_XIE <joy8963259@gmail.com>
2024-04-12 16:28:44 +0800

    chore: update example

--- android_example/app/src/main/java/com/example/android_example/KgSDKService.kt
+++ android_example/app/src/main/java/com/example/android_example/KgSDKService.kt
@@ -17,13 +17,12 @@ class KgSDKService private constructor(private val context: Context) {
     private var flutterEngine: FlutterEngine? = null
     private val channelName = "com.kryptogo.sdk/channel"
 
+
     companion object {
         private var instance: KgSDKService? = null
-
         fun getInstance(@NonNull context: Context): KgSDKService {
             if (instance == null) {
                 instance = KgSDKService(context)
-
             }
             return instance!!
         }
@@ -41,6 +40,8 @@ class KgSDKService private constructor(private val context: Context) {
             FlutterEngineCache
                 .getInstance()
                 .put("flutter_engine", flutterEngine!!)
+            val channel = MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, channelName)
+            this.methodChannel = channel
         }
     }
 
@@ -48,11 +49,43 @@ class KgSDKService private constructor(private val context: Context) {
 
 
     private fun initializeFlutterActivity(flutterEngine: FlutterEngine) {
-        val channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName)
         flutterActivity  = CustomFlutterActivity()
-        this.methodChannel = channel
     }
 
+    fun fetchSharedSecret(): String? {
+        // Get a reference to the shared preferences
+        val sharedPreferences = context.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+        // Return the shared secret from the shared preferences
+        return sharedPreferences.getString("sharedSecret", null)
+    }
+
+    fun updateSharedSecret(sharedSecret: String?): Boolean {
+        // Get a reference to the shared preferences
+        val sharedPreferences = context.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+
+        // Check if the sharedSecret is not null
+        if (sharedSecret != null) {
+            // Start a SharedPreferences editor
+            val editor = sharedPreferences.edit()
+
+            // Put the shared secret into the shared preferences
+            editor.putString("sharedSecret", sharedSecret)
+
+            // Apply changes asynchronously
+            editor.apply()
+
+            // Return true indicating the sharedSecret was updated successfully
+            return true
+        } else {
+            // Optionally handle the case where sharedSecret is null, e.g., by removing the entry
+            val editor = sharedPreferences.edit()
+            editor.remove("sharedSecret")
+            editor.apply()
+
+            // Return false to indicate no update was made due to null input
+            return false
+        }
+    }
 
     fun setOpenVerifyPageCallback(callback: ((result: MethodChannel.Result) -> Unit)?) {
         openVerifyPageCallback = callback
@@ -69,8 +102,8 @@ class KgSDKService private constructor(private val context: Context) {
         context.startActivity(intent)
     }
 
-    fun callKgSDK() {
-        methodChannel?.invokeMethod("testFuture", null, object : MethodChannel.Result {
+    fun callKgSDK(funcName: String): String{
+        methodChannel?.invokeMethod(funcName, null, object : MethodChannel.Result {
             override fun success(@Nullable result: Any?) {
                 print(result)
             }
@@ -81,12 +114,117 @@ class KgSDKService private constructor(private val context: Context) {
                 @Nullable errorDetails: Any?
             ) {
                 print("error")
-
             }
 
             override fun notImplemented() {
                 print("not implemented")
             }
         })
+    }
+}
+
+class CustomFlutterActivity : FlutterActivity() {
+    private val channelName = "com.kryptogo.sdk/channel"
+    // Assume sharedSecret is a nullable String that might have been obtained elsewhere
+    var sharedSecret: String? = null
+    var YOUR_CLIENT_ID = "def3b0768f8f95ffa0be37d0f54e2064"
+    var YOUR_CLIENT_TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImRlbW8ta2V5IiwidHlwIjoiSldUIn0.eyJhdWQiOiJodHRwczovL2tyeXB0b2dvLmNvbSIsImV4cCI6MjAyNzQwOTg2MiwiaXNzIjoiaHR0cHM6Ly9hdXRoLmtyeXB0b2dvLmNvbSIsInN1YiI6InRlc3QtdXNlciJ9.Kmbblm_cUJNpoRImSRQmb83ljY35Kn-ZcA5SBy5WOPqqL6T42YVDJFMyOAp05j3aFfUIZxCOqQAFuT23bC53jZM9SOZjz9cmwqHOE6D9wzk6Y2gwdOABSIeEet2nGzXfoHcPR1GLXJYdnOWYdh9ZivE4dtH4wGRO-eiOUoJX_kxSunBk1XanG6T3BcCDduEd-jxHTBSoi2fcMU_KfDVA9ZTc3kwzzYq3qQUMu8lBIBUQYqeV3S4M29AMn1gUAlP5Z1oKuQZzYEM3jLxAkN9hls1fMavsfi2VGYK87UE7THyWmTgMU9BDNzk3DrT7Wcxc1DOhwotyrTtep8BQkjsCJw"
+
+    companion object {
+        fun withCachedEngine(engineId: String) = CustomCachedEngineIntentBuilder(engineId)
+    }
+
+    class CustomCachedEngineIntentBuilder(engineId: String) :
+        CachedEngineIntentBuilder(CustomFlutterActivity::class.java, engineId)
+
+    fun fetchSharedSecret(): String? {
+        // Get a reference to the shared preferences
+        val sharedPreferences = context.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+        // Return the shared secret from the shared preferences
+        return sharedPreferences.getString("sharedSecret", null)
+    }
+
+    fun updateSharedSecret(sharedSecret: String?): Boolean {
+        // Get a reference to the shared preferences
+        val sharedPreferences = context.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+
+        // Check if the sharedSecret is not null
+        if (sharedSecret != null) {
+            // Start a SharedPreferences editor
+            val editor = sharedPreferences.edit()
+
+            // Put the shared secret into the shared preferences
+            editor.putString("sharedSecret", sharedSecret)
+
+            // Apply changes asynchronously
+            editor.apply()
+
+            // Return true indicating the sharedSecret was updated successfully
+            return true
+        } else {
+            // Optionally handle the case where sharedSecret is null, e.g., by removing the entry
+            val editor = sharedPreferences.edit()
+            editor.remove("sharedSecret")
+            editor.apply()
+
+            // Return false to indicate no update was made due to null input
+            return false
+        }
+    }
+
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        sharedSecret = fetchSharedSecret()
+        val channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName)
+        var initParam: MutableMap<String, Any> = mutableMapOf(
+            "appName" to "TWMTEST",
+            "walletConfig" to mapOf(
+                "maxWallet" to 100,
+                "enableAutoSssSignUp" to true,
+                "enableSSS" to true,
+                "allRpcs" to listOf("ethereum")
+            ),
+            "themeData" to mapOf(
+                "primaryValue" to "FFFFC211"
+            ),
+            "flavorEnum" to "dev", // Initial flavor setting
+            "clientId" to "$YOUR_CLIENT_ID",
+            "clientToken" to "$YOUR_CLIENT_TOKEN"
+        )
+
+        // Conditionally add the sharedSecret if it is not null
+        sharedSecret?.let {
+            initParam["sharedSecret"] = it
+        }
+
+        channel.setMethodCallHandler { call, result ->
+            when (call.method) {
+                "closeFlutterView" -> {
+                    closeFlutterView()
+                    result.success(null)
+                }
+                "openVerifyPage" -> {
+                    // Implement another method call
+                    result.success(true)
+                }
+                "updateSharedSecret" -> {
+                    val sharedSecret = (call.arguments as? Map<String, String>)?.get("sharedSecret")
+                    val isSuccess = updateSharedSecret(sharedSecret)
+                    result.success(isSuccess)
+                }
+                // Add more cases as necessary
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+
+        channel.invokeMethod("getInitialParam", initParam)
+    }
+
+    private fun closeFlutterView() {
+        finish()
     }
 }
--- android_example/app/src/main/java/com/example/android_example/MainActivity.kt
+++ android_example/app/src/main/java/com/example/android_example/MainActivity.kt
@@ -97,9 +97,9 @@ fun Greeting(name: String) {
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Icon(
-                imageVector = Icons.Default.Face, // 使用Material Design 3自带的图标
-                contentDescription = "Favorite", // 为辅助功能提供描述
-                modifier = Modifier.size(24.dp) // 根据需要调整图标大小
+                imageVector = Icons.Default.Face,
+                contentDescription = "Favorite",
+                modifier = Modifier.size(24.dp)
             )
             Text(text = "Hello $name!")
         }
@@ -148,108 +148,3 @@ fun DefaultPreview() {
     }
 }
 
-class CustomFlutterActivity : FlutterActivity() {
-    private val channelName = "com.kryptogo.sdk/channel"
-    // Assume sharedSecret is a nullable String that might have been obtained elsewhere
-    var sharedSecret: String? = null
-    var YOUR_CLIENT_ID = "def3b0768f8f95ffa0be37d0f54e2064"
-    var YOUR_CLIENT_TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImRlbW8ta2V5IiwidHlwIjoiSldUIn0.eyJhdWQiOiJodHRwczovL2tyeXB0b2dvLmNvbSIsImV4cCI6MjAyNzQwOTg2MiwiaXNzIjoiaHR0cHM6Ly9hdXRoLmtyeXB0b2dvLmNvbSIsInN1YiI6InRlc3QtdXNlciJ9.Kmbblm_cUJNpoRImSRQmb83ljY35Kn-ZcA5SBy5WOPqqL6T42YVDJFMyOAp05j3aFfUIZxCOqQAFuT23bC53jZM9SOZjz9cmwqHOE6D9wzk6Y2gwdOABSIeEet2nGzXfoHcPR1GLXJYdnOWYdh9ZivE4dtH4wGRO-eiOUoJX_kxSunBk1XanG6T3BcCDduEd-jxHTBSoi2fcMU_KfDVA9ZTc3kwzzYq3qQUMu8lBIBUQYqeV3S4M29AMn1gUAlP5Z1oKuQZzYEM3jLxAkN9hls1fMavsfi2VGYK87UE7THyWmTgMU9BDNzk3DrT7Wcxc1DOhwotyrTtep8BQkjsCJw"
-
-    companion object {
-        fun withCachedEngine(engineId: String) = CustomCachedEngineIntentBuilder(engineId)
-    }
-
-    class CustomCachedEngineIntentBuilder(engineId: String) :
-        CachedEngineIntentBuilder(CustomFlutterActivity::class.java, engineId)
-
-    fun fetchSharedSecret(): String? {
-        // Get a reference to the shared preferences
-        val sharedPreferences = context.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
-        // Return the shared secret from the shared preferences
-        return sharedPreferences.getString("sharedSecret", null)
-    }
-
-    fun updateSharedSecret(sharedSecret: String?): Boolean {
-        // Get a reference to the shared preferences
-        val sharedPreferences = context.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
-
-        // Check if the sharedSecret is not null
-        if (sharedSecret != null) {
-            // Start a SharedPreferences editor
-            val editor = sharedPreferences.edit()
-
-            // Put the shared secret into the shared preferences
-            editor.putString("sharedSecret", sharedSecret)
-
-            // Apply changes asynchronously
-            editor.apply()
-
-            // Return true indicating the sharedSecret was updated successfully
-            return true
-        } else {
-            // Optionally handle the case where sharedSecret is null, e.g., by removing the entry
-            val editor = sharedPreferences.edit()
-            editor.remove("sharedSecret")
-            editor.apply()
-
-            // Return false to indicate no update was made due to null input
-            return false
-        }
-    }
-    
-
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-
-        sharedSecret = fetchSharedSecret()
-        val channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName)
-        var initParam: MutableMap<String, Any> = mutableMapOf(
-            "appName" to "TWMTEST",
-            "walletConfig" to mapOf(
-                "maxWallet" to 100,
-                "enableAutoSssSignUp" to true,
-                "enableSSS" to true,
-                "allRpcs" to listOf("ethereum")
-            ),
-            "themeData" to mapOf(
-                "primaryValue" to "FFFFC211"
-            ),
-            "flavorEnum" to "dev", // Initial flavor setting
-            "clientId" to "$YOUR_CLIENT_ID",
-            "clientToken" to "$YOUR_CLIENT_TOKEN"
-        )
-
-        // Conditionally add the sharedSecret if it is not null
-        sharedSecret?.let {
-            initParam["sharedSecret"] = it
-        }
-
-        channel.setMethodCallHandler { call, result ->
-            when (call.method) {
-                "closeFlutterView" -> {
-                    closeFlutterView()
-                    result.success(null)
-                }
-                "openVerifyPage" -> {
-                    // Implement another method call
-                    result.success(true)
-                }
-                "updateSharedSecret" -> {
-                    val sharedSecret = (call.arguments as? Map<String, String>)?.get("sharedSecret")
-                    val isSuccess = updateSharedSecret(sharedSecret)
-                    result.success(isSuccess)
-                }
-                // Add more cases as necessary
-                else -> {
-                    result.notImplemented()
-                }
-            }
-        }
-
-        channel.invokeMethod("getInitialParam", initParam)
-    }
-
-    private fun closeFlutterView() {
-        finish()
-    }
-}
